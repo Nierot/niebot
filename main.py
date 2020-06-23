d23dbad98f2c12ded7fd3e9c5984c9938af5a572
Niels <git@nierot.com>
2020-06-23 17:31:15 +0200

    jo

--- main.py
+++ main.py
@@ -2,20 +2,19 @@ import discord
 from discord.ext import commands
 import secrets
 import aiohttp
-from lyricsgenius import Genius
 
 class Bot:
 
     def __init__(self):
-        self.token = secrets.BOT_TOKEN
         self.bot = commands.Bot(command_prefix="yeet ", description="Yeetmeister9000 is here to help!")
+        self.bot.secrets = secrets.Secrets()
+        self.token = self.bot.secrets.BOT_TOKEN
         self.bot.case_insensitive = True
         self.bot._voice_clients = {}
         self.bot._queue = {}
         self.bot._playing = {}
         self.bot._debug = False
         self.extensions = [ 'cogs.admin', 'cogs.mc', 'cogs.pils', 'cogs.text', 'cogs.fun', 'cogs.dromendans'] #,'cogs.youtube', 'cogs.cleverbot']
-        self.genius = Genius(secrets.GENIUS_CLIENT_SECRET)
 
 
     def load_extensions(self):
@@ -39,6 +38,9 @@ class Bot:
         @self.bot.event
         async def on_ready():
             self.bot._session = aiohttp.ClientSession()
+            headers = {}
+            headers['Authorization'] = 'Bearer HzEnNuHDAYJOCIjqLI4kIHzhWIM4nZrkyjgsqLxezhF9DaKQY0ZvqVsMlkT2Zebp'
+            self.bot._genius_session = aiohttp.ClientSession(headers=headers)
             print(self.bot.user.name)
             print(self.bot.user.id)
             print(discord.utils.oauth_url(self.bot.user.id))
